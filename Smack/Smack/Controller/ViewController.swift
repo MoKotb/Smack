0cef26a55cda8e72c731aa64aee20effe6344715
Mohamed Kotb <mo.kotb96@gmail.com>
2019-07-22 02:59:01 +0200

    Setup Project

---
+++ Smack/Smack/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Mohamed Kotb on 7/21/19.
+//  Copyright © 2019 Mohamed Kotb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
